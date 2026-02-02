b0f34ba5960948b921625cdc8dc56ed41ec990d7
Rohan2006717 <rohankemwal12@gmail.com>
2026-02-02 19:00:38 +0000

    basic things

---
+++ 01_basics/variable.js
@@ -0,0 +1,5 @@
+const accountId = 122334
+let accountEmail = "rohankemwal12@gmail.com"
+var accountPassword = "Kiyochi"
+accountCity = "Delhi"
+console.table([accountId,accountEmail,accountPassword.accountCity])
