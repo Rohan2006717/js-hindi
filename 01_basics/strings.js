6eba7dae4eabb549ce1bd4c7140f92306d9edd56
Rohan2006717 <rohankemwal12@gmail.com>
2026-02-03 03:43:47 +0000

    strings basic

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -10,3 +10,16 @@ console.log(gameName.length)
 console.log(gameName.toUpperCase())
 console.log(gameName.charAt(2))
 console.log(gameName.indexOf('f'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+const anotherString = gameName.slice(-4,4); // peeche se start krega or 4 tk jayega and then index 4-1 tk krega
+console.log(anotherString)
+const newStringOne = "   rohan      "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+const url = "https://rohansingh.com/rohan%20singh"
+console.log(url.replace('%20','-'))
+console.log(url.includes('sundar'))
+const gameNamenew = "rohan-fc-madrid"
+console.log(gameNamenew.split('-'))
